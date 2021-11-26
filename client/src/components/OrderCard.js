56287d43af61802a3ec613e722be5976b953fa08
vinicius jesus <vinicius.je99@gmail.com>
2021-11-25 23:25:07 -0300

    frontend: redux remove client order action and reducer

--- client/src/components/OrderCard.js
+++ client/src/components/OrderCard.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import './OrderCard.css'
+import { removeClientOrder } from '../redux/cart'
 
 export default function OrderCard(props){
 
-    const {name, price, quantity, url} = props.item
+    const {id, name, price, quantity, url} = props.item
+    const dispatch = useDispatch()
+
+    const removeOrder = (id) => {
+        dispatch(removeClientOrder(id))
+    }
 
     return(
         <div className='order-container'>
-            <i className="fas fa-times"></i>
+            <i className="fas fa-times" onClick={() => removeOrder(id)}></i>
             <img src={url} alt={name}></img>
             <div className='bar'>
                 <span>{name}</span>
--- client/src/redux/cart/index.js
+++ client/src/redux/cart/index.js
@@ -3,7 +3,9 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 const CLIENT_ORDER = []
 
 export const getClientOrder = createAction('GET_CLIENT_ORDER')
+export const removeClientOrder = createAction('REMOVE_CLIENT_ORDER')
 
 export default createReducer(CLIENT_ORDER, {
-    [getClientOrder]: (state, action) => [...state, action.payload]
+    [getClientOrder]: (state, action) => [...state, action.payload],
+    [removeClientOrder]: (state, action) => state.filter(state => state.id !== action.payload)
 })
