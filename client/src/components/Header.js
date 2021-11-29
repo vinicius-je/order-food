2b432fef02a4edcccec4154a4e0564190a88968d
vinicius jesus <vinicius.je99@gmail.com>
2021-11-29 11:21:16 -0300

    frontend: display order quantity in cart icon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default function Header(){
+
+    const items = useSelector(state => state.cart)
+
+    const displayOrderQuantity = (total, item) => {
+        return total += item.quantity
+    }
+
     return(
         <header>
             <h3>Logo</h3>
@@ -11,7 +19,10 @@ export default function Header(){
                 <li><Link to='/adicionar'>adicionar</Link></li>
                 <li>editar</li>
             </ul>
-            <Link to='/cart'><i className='fas fa-cart-plus'></i></Link>
+            <div className='cart-icon-box'>
+                <div className='counter-circle'>{items.reduce(displayOrderQuantity, 0)}</div>
+                <Link to='/cart'><i className='fas fa-cart-plus'></i></Link>
+            </div>
         </header>
     )
 }
