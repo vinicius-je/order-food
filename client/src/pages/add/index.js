6c4a12dd75fe4da85e6dc540376036563dfca762
vinicius jesus <vinicius.je99@gmail.com>
2021-11-30 23:53:34 -0300

    frontend: set minLength and maxLength in inputs on add page

--- client/src/pages/add/index.js
+++ client/src/pages/add/index.js
@@ -24,19 +24,19 @@ export default function Add(){
         <form className='form-container' onSubmit={onSubmit}>
             <div className='input-container'>
                 <label>Image Url</label>
-                <input name='url' placeholder='Image URL' value={form.url} onChange={formChange}></input>
+                <input name='url' placeholder='Image URL' value={form.url} onChange={formChange} required></input>
             </div>
             <div className='input-container'>
                 <label>Name</label>
-                <input name='name' placeholder='Food name' value={form.name} onChange={formChange}></input>
+                <input name='name' placeholder='Food name' value={form.name} onChange={formChange} minLength='3' maxLength='30' required></input>
             </div>
             <div className='input-container'>
                 <label>Description</label>
-                <input name='description' placeholder='Description' value={form.description} onChange={formChange}></input>
+                <input name='description' placeholder='Description' value={form.description} onChange={formChange} minLength='3' maxLength='100' required></input>
             </div>
             <div className='input-container'>
                 <label>Price</label>
-                <input type='number' name='price' placeholder='Price' max='100' min='0' step='any' value={form.price} onChange={formChange}></input>
+                <input type='number' name='price' placeholder='Price' max='100' min='0' step='any' value={form.price} onChange={formChange} required></input>
             </div>
             <button type="submit" className='btn'>OK</button>
         </form>
