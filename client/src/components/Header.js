f0d6774b8cc3f5a709aba87f0d4d37379c322efc
vinicius-je <vinicius.je99@gmail.com>
2022-03-26 22:25:36 -0300

    frontend: new header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,12 +6,11 @@ import CartSideBar from './CartSideBar'
 import { toggleCartSideBar } from '../redux/cart'
 import './Header.css'
 import { userAuth } from '../redux/auth'
+import Title from './Title'
 
 export default function Header(props){
 
     const orders = useSelector(state => state.orders);
-
-    const [menuSideBar, setMenuSideBar] = useState(false)
 
     const dispatch = useDispatch();
     // display quantity of the orders in cart icon
@@ -25,31 +24,23 @@ export default function Header(props){
         dispatch(userLogout([]));
     }
 
-    useEffect(() => {
-        // get all links btn and and onclick to close menu sidebar
-        let links = document.querySelectorAll('.link-btn');
-        links.forEach(link => { link.onclick = () => setMenuSideBar(!menuSideBar)});
-    }, [menuSideBar]) 
-
     return(
         <>
         <header className='header'>
-            <h3 className='logo'>Logo</h3>
-            <ul className={menuSideBar ? 'navLinks navLinks-active' : 'navLinks'}>
-                <i class="fas fa-times close-menu-sidebar" onClick={() => setMenuSideBar(!menuSideBar)}></i>
-                <Link to='menu'><li className='link-btn'>menu</li></Link>
-                <Link to='add'><li className='link-btn'>add</li></Link>
-                <Link to='edit'><li className='link-btn'>edit</li></Link>
-                <Link to='/' onClick={logout}><li className='link-btn'>logout</li></Link>
-            </ul>
-            <div className='cart-icon-box'>
-                <div className='counter-circle'>{orders.reduce(displayOrderQuantity, 0)}</div>
-                <i className='fas fa-cart-plus' onClick={() => dispatch(toggleCartSideBar())}></i>
+            <Title/>
+            <div className='header-navlinks'>
+                <ul className='navLinks'>
+                    <Link to='menu'><li className='link-btn'>menu</li></Link>
+                    <Link to='add'><li className='link-btn'>add</li></Link>
+                    <Link to='edit'><li className='link-btn'>edit</li></Link>
+                    <Link to='/' onClick={logout}><li className='link-btn'>logout</li></Link>
+                </ul>
             </div>
-            <div className='menu-bg-icon' onClick={() => setMenuSideBar(!menuSideBar)}>
-                <div className='bar'></div>
-                <div className='bar'></div>
-                <div className='bar'></div>
+            <div className='cart-icon-box'>
+                <div className='cart-icon'>
+                    <div className='counter-circle'>{orders.reduce(displayOrderQuantity, 0)}</div>
+                    <i className='fas fa-cart-plus' onClick={() => dispatch(toggleCartSideBar())}></i>
+                </div>
             </div>
             <CartSideBar/>
         </header>
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Title(){
     const style = {
         fontFamily: 'Lobster, cursive',
-        textAlign: 'center',
     }
     return(
         <h1 style={style}>Order Food</h1>
